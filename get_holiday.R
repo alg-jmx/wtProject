7e09c5176e20978cb3a4f9993089a755a82c8ae5
jumingxing <jumingxing@puhuifinance.com>
2016-11-03 18:31:40 +0800

    封装，添加注释

--- get_holiday.R
+++ get_holiday.R
@@ -1,25 +1,34 @@
 rm(list = ls());gc()
-require(RCurl)
-is.holiday = function(x) {
-  # x is a string, e.g. x = "20130101"
-  url.x = paste("http://www.easybots.cn/api/holiday.php?d=", x, sep = "")
-  temp = gsub("\"", "", getURL(url.x))
-  sig = substr(temp, 11, 11)
-  if(sig == "0") return("工作日")
-  if(sig == "1") return("周末")
-  if(sig == "2") return("小长假")
+GetDateDiff = function(startDate, endDate) {
+  suppressMessages(require(RCurl))
+  is.holiday = function(date) {
+    # Judge a date if "工作日"
+    # 
+    # Args:
+    #   date: A string, e.g. date = "20160101" / "2016-01-01" / "2016/01/01"
+    # 
+    # Returns:
+    #   The result of the date given is "工作日", "周末", "小长假"
+    
+    url = paste("http://www.easybots.cn/api/holiday.php?d=", date, sep = "")
+    temp = gsub("\"", "", getURL(url))
+    sig = substr(temp, 11, 11)
+    if(sig == "0") return("工作日")
+    if(sig == "1") return("周末")
+    if(sig == "2") return("小长假")
+  }
+  start = as.Date(startDate)
+  end = as.Date(endDate)
+  except = c("工作日")
+  result = c()
+  for (elem in start:end) {
+    tempDate = as.Date(elem, origin = "1970-01-01")
+    result = append(result, sum(is.holiday(tempDate) %in% except))
+  }
+  # cat("工作日总计:",sum(result),"\n")
+  return(sum(result))
 }
 
-start = as.Date("2016-02-01")
-end = as.Date("2016-02-29")
-except = c("工作日")
-result = c()
-for (date in start:end) {
-  tempDate = as.Date(date, origin = "1970-01-01")
-  result = append(result, sum(is.holiday(tempDate) %in% except))
-  cat("日期是:",is.holiday(tempDate),"\n")
-}
-cat("工作日总计:",sum(result),"\n")
 
 
 
